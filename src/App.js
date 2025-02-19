ca53db98cb1fe312f315b23abf0f1d70817bc34c
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2025-02-18 23:49:50 -0500

    worked on repetition exercise and app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import RepetitionExercise from "./components/RepetitionExercise/index.js";
+
+const exercise = [
+  { name: "Push-Ups", type: "repetition" },
+  { name: "Running", type: "duration" },
+  { name: "Plank", type: "duration" },
+]; 
 
 function App() {
+
+  const [selectExercise, setSelectExercise] = useState(null);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {!selectExercise ? (
+      <h1>no exercise selected</h1>
+      ) : (
+          <div>
+            <RepetitionExercise />
+          </div>
+      )
+      };
     </div>
   );
-}
+};
 
 export default App;
